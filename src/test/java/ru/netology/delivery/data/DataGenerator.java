0d1d1b358b939aa3149ed46ed5e72f36ea397f9b
swesda <swesda@yandex.ru>
2022-09-13 21:16:00 +0300

    Second Commit

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,13 +2,10 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.experimental.UtilityClass;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.Random;
 
 public class DataGenerator {
     private DataGenerator() {
